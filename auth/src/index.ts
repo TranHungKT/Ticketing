8753e69b3e61da4bd9fbf81741d6f135a7d91bce
Trần Mạnh Hưng <tranhung26122612@gmail.com>
2021-07-18 00:06:01 +0700

    ingress

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,16 @@ import express from 'express'
 const app = express();
 app.use(express.json());
 
+app.get('/api/users/currentUser', (req,res) => {
+    console.log('adsasd')
+    res.send("HI there")
+})
+
+app.get('/', (req,res) =>{
+    res.send("OK")
+})
+
 app.listen(3000, () => {
+    console.log("////")
     console.log("Listening on port 3000")
 })
